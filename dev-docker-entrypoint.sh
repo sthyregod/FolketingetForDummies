972360be243b45dc8d94ca43359beb045634dd05
Simon Thyregod <git@simon.thyregod.eu>
2023-08-26 07:51:32 +0200

    Update gems + change default run command

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -9,6 +9,6 @@ rm -f "$APP_PATH/tmp/pids/server.pid"
 
 # run passed commands
 # Run with 'debug' gem
-bundle exec rdbg -O --port 1234 -- bin/rails s -b 0.0.0.0
+# bundle exec rdbg -O --port 1234 -- bin/rails s -b 0.0.0.0
 # Run without debugging
-# bin/rails s -b 0.0.0.0
+bin/rails s -b 0.0.0.0
